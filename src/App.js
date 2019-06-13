ed7cbdf4a3766828c354b8acbcb26b63ead1cd7d
Preeti <singhp1992@gmail.com>
2019-06-13 17:51:27 +0200

    fixing issues

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { render } from "react-dom";
 import Tabs from './components/Tabs';
 
 class App extends Component {
+
   render() {
     return (        
       <section>
@@ -13,60 +14,14 @@ class App extends Component {
         </header>
           <div class="contact-cards">
             <Tabs>
-              <div label="A">
+              <div label="A"> 
                 All the A's
               </div>
               <div label="B">
                 All the B's
-              </div>
+              </div> 
               <div label="C">
                 All the C's
-              </div>
-              <div label="D">
-              </div>
-              <div label="E">
-              </div>
-              <div label="F">
-              </div>
-              <div label="G">
-              </div>
-              <div label="H">
-              </div>
-              <div label="I">
-              </div>
-              <div label="J">
-              </div>
-              <div label="K">
-              </div>
-              <div label="L">
-              </div>
-              <div label="M">
-              </div>
-              <div label="N">
-              </div>
-              <div label="O">
-              </div>
-              <div label="P">
-              </div>
-              <div label="Q">
-              </div>
-              <div label="R">
-              </div>
-              <div label="S">
-              </div>
-              <div label="T">
-              </div>
-              <div label="U">
-              </div>
-              <div label="V">
-              </div>
-              <div label="W">
-              </div>
-              <div label="X">
-              </div>
-              <div label="Y">
-              </div>
-              <div label="Z">
               </div>
             </Tabs>
           </div>
