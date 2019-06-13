8a6538937b5a666252d106f425c42f8170aa0514
Preeti <singhp1992@gmail.com>
2019-06-13 18:50:55 +0200

    maping through the alphabet

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import './App.css';
 import { render } from "react-dom";
 
 import Tabs from './components/Tabs';
+import Tab from './components/Tab'
 
 class App extends Component {
+  state= {
+    alphabets:[
+      { name:"A" },{ name: "B" },{ name: "C" },{ name: "D"},
+      { name: "E" }, { name: "F" }, { name: "G" }, { name: "H" },
+      { name: "I" }, { name: "J" }, { name: "K" }, { name: "L" },
+      { name: "M" }, { name: "N" }, { name: "O" }, { name: "P" },
+      { name: "Q" }, { name: "R" }, { name: "S" }, { name: "T" },
+      { name: "U" }, { name: "V" }, { name: "W" }, { name: "X" },
+      { name: "Y" }, { name: "Z" }
+    ]
+  }
+
 
   render() {
     return (        
@@ -14,15 +27,9 @@ class App extends Component {
         </header>
           <div class="contact-cards">
             <Tabs>
-              <div label="A"> 
-                All the A's
-              </div>
-              <div label="B">
-                All the B's
-              </div> 
-              <div label="C">
-                All the C's
-              </div>
+                {this.state.alphabets.map ( alphabet =>
+                <Tab label={alphabet.name}/>
+                )}
             </Tabs>
           </div>
 
